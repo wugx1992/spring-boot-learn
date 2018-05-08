0293d6832bd8795578946fbfa29fb0fc398e2520
gx.wu <1807151593@qq.com>
2018-05-08 16:08:20 +0800

    first commit

---
+++ spring-boot-mybatis/src/main/java/indi/gxwu/mybatis/MyBatisApplication.java
@@ -0,0 +1,11 @@
+package indi.gxwu.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MyBatisApplication.class,args);
+    }
+}
